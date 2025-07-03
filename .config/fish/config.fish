a637d6b6bf4427b29a4329c89cd4fc77a3250666
boubli <49991411+boubl@users.noreply.github.com>
2025-07-03 20:02:45 +0200

    add a bunch of tweaks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,28 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -x GOPATH (go env GOPATH)
-set -x -a PATH (go env GOPATH)/bin
-
-# bun
-set -x BUN_INSTALL "$HOME/.bun"
-set -x -a PATH $BUN_INSTALL/bin
-
-# zigup
-set -x ZIGUP_INSTALL "$HOME/zig"
-set -x -a PATH $ZIGUP_INSTALL/
-
 # Homebrew settings
 set -x HOMEBREW_NO_ENV_HINTS 1
+set -x GSK_RENDERER ngl
 
 # Zoxide
 zoxide init fish | source
 alias cd=z
-
-# BEGIN opam configuration
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
-# test -r '/Users/boubli/.opam/opam-init/init.fish' && source '/Users/boubli/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
-# END opam configuration
