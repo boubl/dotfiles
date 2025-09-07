348c4e13ffc64b85fdb4e8f01f60cb90de412a82
boubli <49991411+boubl@users.noreply.github.com>
2025-09-07 23:02:52 +0200

    add starship and fastfetch, plus some tweaks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # set ZELLIJ_AUTO_ATTACH true
+    # set ZELLIJ_AUTO_EXIT true
+    # eval (zellij setup --generate-auto-start fish | string collect)
+
+    # Starship
+    starship init fish | source
+
+    # Zoxide
+    zoxide init fish | source
+    alias cd=z
 end
 
 # Homebrew settings
 set -x HOMEBREW_NO_ENV_HINTS 1
-
-# Starship
-starship init fish | source
-
-# Zoxide
-zoxide init fish | source
-alias cd=z
